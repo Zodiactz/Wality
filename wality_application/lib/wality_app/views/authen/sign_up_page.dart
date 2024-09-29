d26f5439c3c6f04d828e792db121c82d7631c603
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2024-09-29 16:21:49 +0700

    update reward page

--- wality_application/lib/wality_app/views/authen/sign_up_page.dart
+++ wality_application/lib/wality_app/views/authen/sign_up_page.dart
@@ -131,7 +131,8 @@ class _authenPageState extends State<SignUpPage> {
               totalMl: 0,
               botLiv: 0,
               profileImg_link: "",
-              fillingLimit: 0);
+              fillingLimit: 0,
+              eventBot: 0);
 
           // Call the service to create the user and handle the response
           final result = await _authService.createUser(newUser);
