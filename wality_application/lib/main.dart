3960f099038112ea4acf17a73e3f557f837f301c
Zodiact <98137596+Zodiactz@users.noreply.github.com>
2024-10-01 13:48:55 +0700

    fixing email

--- wality_application/lib/main.dart
+++ wality_application/lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Wality',
       theme: ThemeData(),
-      home:  /*LogoPage()*/const HomePage(),
+      home:  LogoPage()/*const HomePage()*/,
       routes: {
         '/logopage': (context) => const LogoPage(),
         '/choosewaypage': (context) => const ChooseWayPage(),
