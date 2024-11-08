4a7394627b40cf16f6365a6c5fd3f3621b23abad
Ratchanon Burong <Non20220@hotmail.com>
2024-11-08 19:50:33 +0700

    update

--- wality_application/lib/wality_app/utils/pop_over_water.dart
+++ wality_application/lib/wality_app/utils/pop_over_water.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wality_application/wality_app/utils/navigator_utils.dart';
+import 'package:wality_application/wality_app/views/waterCheck/ntu_checking_page.dart';
 
 class PopOverForWater extends StatelessWidget {
   const PopOverForWater({super.key});
+
+  Future<bool> _shouldShowTutorial() async {
+    final prefs = await SharedPreferences.getInstance();
+    return !(prefs.getBool('water_tutorial_seen') ?? false);
+  }
+
+  void _showTutorial(BuildContext context) async {
+    if (await _shouldShowTutorial()) {
+      if (context.mounted) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => WaterTutorialPopup(
+            onComplete: () {
+              Navigator.pop(context);
+              openWaterCheckingPage(context);
+            },
+          ),
+        );
+      }
+    } else {
+      openWaterCheckingPage(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GestureDetector(
-          onTap: () {
-            Future.delayed(
-              const Duration(),
-              () => openWaterCheckingPage(
-                  context), // Replace with camera function
-            );
-          },
+          onTap: () => _showTutorial(context),
           child: Container(
             height: 50,
             color: Colors.blue[500],
@@ -23,9 +43,10 @@ class PopOverForWater extends StatelessWidget {
               child: Text(
                 'Water Clearness Reader',
                 style: TextStyle(
-                  color: Colors.white, // Text color
-                  fontSize: 16, // Font size
-                  fontWeight: FontWeight.bold, // Font weight
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed',
                 ),
               ),
             ),
@@ -35,8 +56,7 @@ class PopOverForWater extends StatelessWidget {
           onTap: () {
             Future.delayed(
               const Duration(),
-              () => openQRcodePage(
-                  context), // Replace with camera function
+              () => openQRcodePage(context),
             );
           },
           child: Container(
@@ -46,9 +66,10 @@ class PopOverForWater extends StatelessWidget {
               child: Text(
                 'QR Collect Scanner',
                 style: TextStyle(
-                  color: Colors.white, // Text color
-                  fontSize: 16, // Font size
-                  fontWeight: FontWeight.bold, // Font weight
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed',
                 ),
               ),
             ),
