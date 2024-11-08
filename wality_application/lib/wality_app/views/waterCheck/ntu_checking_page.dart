b266afa444a54b23cb71cb4fb7a94486de8323e8
Zodiact <98137596+Zodiactz@users.noreply.github.com>
2024-11-09 06:20:02 +0700

    nuclear update

--- wality_application/lib/wality_app/views/waterCheck/ntu_checking_page.dart
+++ wality_application/lib/wality_app/views/waterCheck/ntu_checking_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wality_application/wality_app/utils/LoadingOverlay.dart';
 import 'package:wality_application/wality_app/utils/navigator_utils.dart';
 import 'package:wality_application/wality_app/views_models/water_checking_vm.dart';
 
@@ -16,20 +17,22 @@ class WaterCheckingPage extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return ChangeNotifierProvider(
-        create: (_) => WaterCheckingViewModel(image),
-        child: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Color(0xFF0083AB), Color(0xFF003545)],
-              ),
-            ),
-            child: SafeArea(
-              child: Consumer<WaterCheckingViewModel>(
-                builder: (context, watercheckingvm, child) {
-                  return Column(
+      create: (_) => WaterCheckingViewModel(image),
+      child: Consumer<WaterCheckingViewModel>(
+        builder: (context, watercheckingvm, child) {
+          return LoadingOverlay(
+            isLoading: watercheckingvm.isLoading,
+            child: Scaffold(
+              body: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xFF0083AB), Color(0xFF003545)],
+                  ),
+                ),
+                child: SafeArea(
+                  child: Column(
                     children: [
                       _buildAppBar(context),
                       Expanded(
@@ -49,12 +52,14 @@ class WaterCheckingPage extends StatelessWidget {
                         ),
                       ),
                     ],
-                  );
-                },
+                  ),
+                ),
               ),
             ),
-          ),
-        ));
+          );
+        },
+      ),
+    );
   }
 
   Widget _buildAppBar(BuildContext context) {
@@ -167,196 +172,220 @@ class WaterCheckingPage extends StatelessWidget {
     );
   }
 
-  Future<void> _handleImageUploadAndShowResult(BuildContext context) async {
-    // Show loading dialog
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return const Center(
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-          ),
-        );
-      },
-    );
+   Future<void> _handleImageUploadAndShowResult(BuildContext context) async {
+    final viewModel = Provider.of<WaterCheckingViewModel>(context, listen: false);
+    viewModel.setLoading(true);
 
     try {
       final userService = UserService();
       final uploadedUrl = await userService.uploadImage(image);
 
-      // Close loading dialog
-      Navigator.pop(context);
-
       if (uploadedUrl != null) {
-        // Call the inference method with the uploaded image URL
-        final inferenceResult =
-            await userService.runRoboflowInference(uploadedUrl);
+        final inferenceResult = await userService.runRoboflowInference(uploadedUrl);
         print(uploadedUrl);
 
+        viewModel.setLoading(false);
+
         if (inferenceResult != null && inferenceResult['outputs'] != null) {
-          // Show the inference results in the result dialog
-          _showResultDialog(context, uploadedUrl, inferenceResult);
-          print(inferenceResult);
+          final ntuLevel = _extractNtuLevel(inferenceResult);
+          _showResultDialog(context, uploadedUrl, ntuLevel);
         } else {
-          // Show error dialog if inference fails
           _showErrorDialog(context);
         }
       } else {
+        viewModel.setLoading(false);
         _showErrorDialog(context);
       }
     } catch (e) {
-      Navigator.pop(context);
+      viewModel.setLoading(false);
       _showErrorDialog(context);
     }
   }
 
-  void _showResultDialog(BuildContext context, String uploadedUrl,
-      Map<String, dynamic> inferenceResult) {
+  String _extractNtuLevel(Map<String, dynamic> inferenceResult) {
+    try {
+      if (inferenceResult['outputs'] != null &&
+          inferenceResult['outputs'].isNotEmpty) {
+        final outputs = inferenceResult['outputs'][0];
+        print(outputs);
+
+        if (outputs['predictions'] != null &&
+            outputs['predictions']['predictions'] is List) {
+          final predictions = outputs['predictions']['predictions'];
+          print(predictions);
+
+          // Sort predictions by confidence in descending order
+          predictions.sort((a, b) =>
+              (b['confidence'] as num).compareTo(a['confidence'] as num));
+
+          // Look for NTU class in predictions
+          for (var prediction in predictions) {
+            if (prediction['class'].toString().startsWith('NTU_')) {
+              // Extract the number after 'NTU_'
+              return prediction['class'].toString().replaceAll('NTU_', '');
+            }
+          }
+        }
+      }
+      print("No NTU prediction found in result: $inferenceResult");
+      return 'N/A';
+    } catch (e) {
+      print("Error extracting NTU level: $e");
+      print("Result structure: $inferenceResult");
+      return 'N/A';
+    }
+  }
+
+  void _showResultDialog(
+      BuildContext context, String uploadedUrl, String ntuLevel) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    // Determine the result image based on the inference output
-    String resultImage = inferenceResult['outputs'] != null
-        ? 'assets/images/Good_water.png'
-        : 'assets/images/No_water.png';
-    String resultText = inferenceResult['resultText'] ?? 'No result available';
-  
-    if (resultText == 'No result available') {
+    // Determine the result image, text, and colors based on the NTU level
+    String resultImage;
+    String resultText;
+    Color resultTextColor;
+    List<Color> gradientColors; // Add gradient colors variable
+
+    if (ntuLevel == 'N/A') {
       resultImage = 'assets/images/No_water.png';
-    }
-    if (resultText == 'Good water') {
-      resultImage = 'assets/images/Good_water.png';
+      resultText = 'Water not detected!';
+      resultTextColor = Colors.grey;
+      gradientColors = const [Colors.grey, Colors.white];
+    } else {
+      // Convert ntuLevel to double for comparison
+      double ntuValue = double.tryParse(ntuLevel) ?? 0;
+
+      if (ntuValue > 5) {
+        resultImage = 'assets/images/Bad_water.png';
+        resultText = 'The water has a bad clearness!';
+        resultTextColor = Colors.red;
+        gradientColors = const [
+          Colors.red,
+          Colors.white
+        ]; // Red gradient for bad results
+      } else {
+        resultImage = 'assets/images/Good_water.png';
+        resultText = "The water has a good clearness!";
+        resultTextColor = const Color(0xFF0083AB);
+        gradientColors = const [
+          Color(0xFF0083AB),
+          Colors.white
+        ]; // Blue gradient for good results
+      }
     }
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          elevation: 8,
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
+        return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+          child: Dialog(
+            shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20.0),
-              gradient: const LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFF0083AB),
-                  Colors.white,
-                ],
-              ),
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: const Text(
-                    "Water Clearness Result",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: 'RobotoCondensed',
+            elevation: 8,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: gradientColors, // Use dynamic gradient colors
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: const Text(
+                      "Water Clearness Result",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'RobotoCondensed',
+                      ),
                     ),
                   ),
-                ),
-                SingleChildScrollView(
+                  SingleChildScrollView(
                     child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 20),
-                  child: Column(children: [
-                    Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: const Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Image.asset(
-                        resultImage,
-                        width: screenWidth * 0.2,
-                        height: screenWidth * 0.2,
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
-                            spreadRadius: 1,
-                            blurRadius: 3,
-                            offset: const Offset(0, 2),
-                          ),
-                        ],
-                      ),
+                      margin: const EdgeInsets.symmetric(vertical: 20),
                       child: Column(
                         children: [
-                          Text(
-                            resultText,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: Color(0xFF0083AB),
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'RobotoCondensed',
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Image.asset(
+                              resultImage,
+                              width: screenWidth * 0.2,
+                              height: screenWidth * 0.2,
+                              fit: BoxFit.contain,
                             ),
                           ),
-                          Text(
-                            "The tubidity of the water is 0.5 NTU",
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: Color(0xFF0083AB),
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'RobotoCondensed',
+                          const SizedBox(height: 20),
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 1,
+                                  blurRadius: 3,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
                             ),
-                          ),
-                          /*const SizedBox(height: 15),
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 20,
-                                  vertical: 8,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFF0083AB),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Text(
-                                  accuracy,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
+                            child: Column(
+                              children: [
+                                Text(
+                                  resultText,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: resultTextColor,
+                                    fontSize: 22,
                                     fontWeight: FontWeight.bold,
                                     fontFamily: 'RobotoCondensed',
                                   ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(height: 20),
+                                Text(
+                                  "The turbidity of the water is around $ntuLevel NTU",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 22,
+                                    fontFamily: 'RobotoCondensed',
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),*/
                           const SizedBox(height: 24),
                           ElevatedButton(
                             onPressed: () {
                               _handleImageDeletion(context, uploadedUrl);
                             },
                             style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFF0083AB),
+                              backgroundColor:
+                                  resultTextColor, // Use the result color for the button
                               foregroundColor: Colors.white,
                               padding: const EdgeInsets.symmetric(
                                 horizontal: 40,
@@ -379,9 +408,9 @@ class WaterCheckingPage extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ]),
-                ))
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
--- wality_application/lib/wality_app/views_models/water_checking_vm.dart
+++ wality_application/lib/wality_app/views_models/water_checking_vm.dart
@@ -4,29 +4,28 @@ import 'package:image_picker/image_picker.dart';
 import 'package:tflite_v2/tflite_v2.dart';
 import 'package:wality_application/wality_app/models/waterquality.dart';
 
-
 class WaterCheckingViewModel extends ChangeNotifier {
   File image;
   List<WaterQualityRecognition> recognitions = [];
   String filteredResults = "";
-  
+  bool _isLoading = false;
 
   WaterCheckingViewModel(this.image) {
-    _loadModel();
     detectImage(image);
   }
 
-  Future<void> _loadModel() async {
-    await Tflite.loadModel(
-      model: "assets/model/best_float32.tflite",
-      labels: "assets/model/label.txt",
-    );
+  bool get isLoading => _isLoading;
+  void setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
   }
+
 
   Future<void> pickImage() async {
     try {
       final ImagePicker picker = ImagePicker();
-      final XFile? pickedFile = await picker.pickImage(source: ImageSource.camera);
+      final XFile? pickedFile =
+          await picker.pickImage(source: ImageSource.camera);
       if (pickedFile != null) {
         image = File(pickedFile.path);
         detectImage(image);
@@ -47,15 +46,17 @@ class WaterCheckingViewModel extends ChangeNotifier {
     );
 
     recognitions = recognitionsList?.map((recognition) {
-      return WaterQualityRecognition(
-        label: recognition['label'],
-        confidence: recognition['confidence'],
-      );
-    }).toList() ?? [];
+          return WaterQualityRecognition(
+            label: recognition['label'],
+            confidence: recognition['confidence'],
+          );
+        }).toList() ??
+        [];
 
     filteredResults = recognitions
         .where((recognition) => recognition.confidence! > 0.7)
-        .map((recognition) => "${recognition.label}: ${(recognition.confidence! * 100).toStringAsFixed(2)}%")
+        .map((recognition) =>
+            "${recognition.label}: ${(recognition.confidence! * 100).toStringAsFixed(2)}%")
         .join("\n");
 
     if (filteredResults.isEmpty) {
