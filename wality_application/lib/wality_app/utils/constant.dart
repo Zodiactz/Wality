2930912715df4002f19bd6c0dbf37419e659bb64
Zodiact <98137596+Zodiactz@users.noreply.github.com>
2024-11-10 11:08:20 +0700

    Revert "update return img"
    This reverts commit bfe2132a0c51998f54b9b44f52a72acfb315dfbf.

--- wality_application/lib/wality_app/utils/constant.dart
+++ wality_application/lib/wality_app/utils/constant.dart
@@ -1,5 +1,2 @@
 const String baseUrl = 'http://localhost:8080';
-const String roboflowUrl =
-    'https://detect.roboflow.com/infer/workflows/kmutt-ai/ntureader';
-const String roboglowUrlButImg =
-    "https://detect.roboflow.com/infer/workflows/kmutt-ai/ntureadershowimg";
+const String roboflowUrl = 'https://detect.roboflow.com/infer/workflows/kmutt-ai/ntureader';
